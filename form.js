1c7289f1a10e7ec6e3d28adf16388c5ef2310647
Mario1379 <122490187+Mario1379@users.noreply.github.com>
2023-03-29 12:27:12 -0600

    Add files via upload

---
+++ form.js
@@ -0,0 +1,13 @@
+function ir() {
+  var user,pass;
+  user =document.getElementById("usuario").value;
+  pass =document.getElementById("contraseña").value;
+  if(user == "jjimenezl", "mmarotor","jbarahonac","ecuadrad","srojasf","kdiazm","ipradon","kbrioness","nespinozaa" && pass == "Ins.2023!"){
+    window.location= "Powerbi.html";
+    alert ("Bienvenido");
+
+  }
+ else{
+  alert("Contraseña incorrecta ");
+ }
+}
